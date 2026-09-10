100194e617160a598937e9ec74b79f5444063fe4
Kyllan <creditimvu03@gmail.com>
2026-09-10 16:47:27 +0000

    bypass build security check

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // Autorise le build sur Vercel même avec cette version de Next.js
   typescript: {
     ignoreBuildErrors: true,
   },
